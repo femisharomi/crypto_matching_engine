f9153ffb51e53e1fd86bad47fdf1060ee6f2cd13
Femi Sharomi <Mrfemi1998@gmail.com>
2026-06-15 01:03:42 +0100

    Create project skeleton with CMake and tests
    Create project skeleton with CMake and tests

---
+++ software/app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "crypto_matching_engine Started" << std::endl;
+
+    std::cout << "crypto_matching_engine Ended!" << std::endl;
+    return 0;
+}
